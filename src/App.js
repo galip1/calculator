a3525ab90ff3ddc0c17925364b64d1b937c07768
galip <galipts1@gmail.com>
2023-03-31 17:32:13 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Calculator from "./components/calculator/calculator";
-import Weather from "./components/weather/weather";
 
 const App = () => {
   return (
     <div>
       <Calculator />
-      <Weather />
     </div>
   );
 };
